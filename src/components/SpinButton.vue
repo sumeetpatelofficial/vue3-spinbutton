2e91f307c4f8b2ed5efe4a33df27c6427aaeb9a9
Sumeet Patel <sumeet.patel@eroam.com>
2021-11-23 12:53:51 +0530

    Updated style for spin button.

--- src/components/SpinButton.vue
+++ src/components/SpinButton.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-defineProps<{ start: number }>();
+const props = defineProps<{ start: number }>();
 
-const count = ref(0);
+const count = ref(props.start);
 </script>
 
 <template>
+<div class="spin-button">
   <button class="btn-negative" @click="count--">
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -18,8 +19,8 @@ const count = ref(0);
     </svg>
   </button>
 
-  <div class="spin-value">
-    <input maxlength="3" type="text" name="" :value="count" id="" />
+  <div class="spin-value">    
+    <label>{{count}}</label>
   </div>
 
   <button class="btn-positive" @click="count++">
@@ -32,6 +33,90 @@ const count = ref(0);
       <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z" />
     </svg>
   </button>
+</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .spin-button {
+    display: flex;
+    min-width: 150px;
+    max-width: 150px;
+    margin: 10px;
+    font-family: inherit;
+  }
+  .spin-button .spin-value {
+    display: block;
+    flex: 1;
+    border: 1px solid #0069ed;
+    border-left: unset;
+    border-right: unset;
+  }
+  .spin-button .spin-value input[type="text"],.spin-button .spin-value input[type="number"]{
+    padding: 0;
+    width: 100%; 
+    height: 100%;
+    border-radius: unset;    
+    box-shadow: none;
+    text-align: center;
+    border: unset;
+  }
+
+  .spin-button .spin-value input[type="text"]:focus,
+  .spin-button .spin-value input[type="number"]:focus{
+    outline:none;
+  }
+
+  .spin-button .spin-value input[type=number]::-webkit-inner-spin-button, 
+  .spin-button .spin-value input[type=number]::-webkit-outer-spin-button { 
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      appearance: none;
+      margin: 0; 
+  }
+
+  .spin-button .spin-value > label{
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+  }
+  .spin-button .btn-negative, .spin-button .btn-positive {
+    display: inline-block;
+    border: none;
+    padding: 0.5rem 1rem;
+    margin: 0;
+    text-decoration: none;
+    background: #0069ed;
+    color: #ffffff;
+    font-family: sans-serif;
+    font-size: 1rem;
+    cursor: pointer;
+    text-align: center;
+    transition: background 250ms ease-in-out, transform 150ms ease;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    flex: 0;
+  }
+  .spin-button .btn-negative:hover, .spin-button .btn-positive:hover, .spin-button .btn-negative:focus, .spin-button .btn-positive:focus {
+    background: #0053ba;
+  }
+  .spin-button .btn-negative:focus, .spin-button .btn-positive:focus {
+    outline: none;
+  }
+  .spin-button .btn-negative:active, .spin-button .btn-positive:active {
+    transform: scale(0.99);
+  }
+  .spin-button .btn-negative svg,
+  .spin-button .btn-positive svg {
+    fill: #ffffff;
+  }
+  .spin-button:first-child {
+    border-top-left-radius: 4px;
+    border-bottom-left-radius: 4px;
+  }
+  .spin-button:last-child {
+    border-top-right-radius: 4px;
+    border-bottom-right-radius: 4px;
+  }
+</style>
